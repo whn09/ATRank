b828055ff8d96067f7ee3d63d6dfcd7cf4328d02
henan.wang <henan.wang@henan.wang's MacBook Pro>
2018-12-06 19:48:32 +0800

    split train and test

--- atrank/build_dataset_funny.py
+++ atrank/build_dataset_funny.py
@@ -5,10 +5,14 @@ import numpy as np
 random.seed(1234)
 
 with open('../raw_data/funny_remap.pkl', 'rb') as f:
-    reviews_df = pickle.load(f)
+    # reviews_df = pickle.load(f)
+    train_reviews_df = pickle.load(f)
+    test_reviews_df = pickle.load(f)
     cate_list = pickle.load(f)
     user_count, item_count, cate_count, example_count = pickle.load(f)
-    print('reviews_df:', reviews_df.shape, reviews_df.head())
+    # print('reviews_df:', reviews_df.shape, reviews_df.head())
+    print('train_reviews_df:', train_reviews_df.shape, train_reviews_df.head())
+    print('test_reviews_df:', test_reviews_df.shape, test_reviews_df.head())
     print('cate_list:', len(cate_list))
     print('user_count, item_count, cate_count, example_count:', user_count, item_count, cate_count, example_count)
     # asin_key, cate_key, revi_key = pickle.load(f)
@@ -28,35 +32,43 @@ def proc_time_emb(hist_t, cur_t):
     return hist_t
 
 
-train_set = []
-test_set = []
-cnt = 0
-for reviewerID, hist in reviews_df.groupby('reviewerID'):
-    if cnt % 1000 == 0:
-        print('cnt:', cnt, '/', user_count)
-    cnt += 1
-    # print('hist:', hist)
-    pos_list = hist[hist.rating == 2]['asin'].tolist()
-    neg_list = hist[hist.rating == 1]['asin'].tolist()
-    print('pos_list:', len(pos_list), pos_list[:5])
-    print('neg_list:', len(neg_list), neg_list[:5])
-    tim_list = hist['unixReviewTime'].tolist()
-    print('tim_list1:', len(tim_list), tim_list[:5])
-    tim_list = [i // 3600 // 24 for i in tim_list]
-    print('tim_list2:', len(tim_list), tim_list[:5])
+def generate_dataset(df):
+    dataset = []
+    cnt = 0
+    user_df = df.groupby('reviewerID')
+    for reviewerID, hist in user_df:
+        if cnt % 1000 == 0:
+            print('cnt:', cnt, '/', user_df.shape[0])
+        cnt += 1
+        # print('hist:', hist)
+        pos_list = hist[hist.rating == 2]['asin'].tolist()
+        neg_list = hist[hist.rating == 1]['asin'].tolist()
+        # print('pos_list:', len(pos_list), pos_list[:5])
+        # print('neg_list:', len(neg_list), neg_list[:5])
+        tim_list = hist['unixReviewTime'].tolist()
+        # print('tim_list1:', len(tim_list), tim_list[:5])
+        tim_list = [i // 3600 // 24 for i in tim_list]
+        # print('tim_list2:', len(tim_list), tim_list[:5])
 
-    for i in range(1, len(pos_list)):
-        hist_i = pos_list[:i]
-        hist_t = proc_time_emb(tim_list[:i], tim_list[i])
-        print('hist_i:', hist_i, 'hist_t:', hist_t)
-        if i != len(pos_list) - 1:
-            train_set.append((reviewerID, hist_i, hist_t, pos_list[i], 1))
-            train_set.append((reviewerID, hist_i, hist_t, neg_list[i], 0))
-        else:
-            label = (pos_list[i], neg_list[i])
-            test_set.append((reviewerID, hist_i, hist_t, label))
+        for i in range(1, len(pos_list)):
+            hist_i = pos_list[:i]
+            hist_t = proc_time_emb(tim_list[:i], tim_list[i])
+            # print('hist_i:', hist_i, 'hist_t:', hist_t)
+            if i != len(pos_list) - 1:
+                dataset.append((reviewerID, hist_i, hist_t, pos_list[i], 1))
 
+        for i in range(1, len(neg_list)):
+            hist_i = neg_list[:i]
+            hist_t = proc_time_emb(tim_list[:i], tim_list[i])
+            # print('hist_i:', hist_i, 'hist_t:', hist_t)
+            if i != len(neg_list) - 1:
+                dataset.append((reviewerID, hist_i, hist_t, neg_list[i], 0))
+    return dataset
+
+
+train_set = generate_dataset(train_reviews_df)
 print('train_set:', len(train_set), train_set[:5])
+test_set = generate_dataset(test_reviews_df)
 print('test_set:', len(test_set), test_set[:5])
 
 random.shuffle(train_set)
